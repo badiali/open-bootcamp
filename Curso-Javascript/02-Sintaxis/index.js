a6ab142ddad057508134248c1bea728c7ef36ff5
Luis Badiali <luis@badiali.es>
2022-07-04 20:00:54 +0200

    Ejercicio 02 - Sintaxis

---
+++ Curso-Javascript/02-Sintaxis/index.js
@@ -0,0 +1,12 @@
+const lista = [
+  "Luis",
+  40,
+  true,
+  new Date("12/11/1981"),
+  {
+    titulo: "Aprender a dibujar",
+    autor: "Betty Edwards",
+    fecha: new Date("1/1/2020"),
+    url: "https://amzn.to/3OD0lWq"
+  }
+];
