0a294316d8659d7b4b1e6a42b8fa6726b498c632
Luis Badiali <luis@badiali.es>
2022-07-04 21:27:57 +0200

    Ejercicio 3 - Estructuras de control

---
+++ Curso-Javascript/03-Estructuras-de-control/Ejercicio/factorial-for.js
@@ -0,0 +1,9 @@
+// Calcular el factorial de 10 con for
+
+let factorial = 10;
+let total;
+
+for (let i = 0; i < factorial; i++) {
+  total = factorial * i;
+  console.log(total);
+}
---
+++ Curso-Javascript/03-Estructuras-de-control/Ejercicio/factorial-while.js
@@ -0,0 +1,11 @@
+// Calcular el factorial de 10 con while
+
+let factorial = 10;
+let total;
+let i = 0;
+
+while (i < factorial) {
+  total = factorial * i;
+  console.log(total);
+  i++;
+}
