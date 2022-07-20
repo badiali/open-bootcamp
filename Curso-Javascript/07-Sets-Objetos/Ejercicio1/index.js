271b90d52a1f58bb880a6f8acdb9266d547903ea
Luis Badiali <luis@badiali.es>
2022-07-20 20:05:26 +0200

    07 Ejercicio 1

---
+++ Curso-Javascript/07-Sets-Objetos/Ejercicio1/index.js
@@ -0,0 +1,8 @@
+const familia = new Set(["Luis", "Meme", "Fabio", "Emma"]);
+console.log(familia);
+
+familia.add("Luis");
+console.log(familia);
+
+familia.add("Javascript");
+console.log(familia);
