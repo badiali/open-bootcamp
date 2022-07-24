85832da47f36fd9a60ce3152b3abe89bf405801c
Luis Badiali <luis@badiali.es>
2022-07-24 13:42:36 +0200

    Corrección del ejercicio 09

--- Curso-Javascript/09-Gestion-de-errores/Ejercicio/index.js
+++ Curso-Javascript/09-Gestion-de-errores/Ejercicio/index.js
@@ -5,21 +5,16 @@ const logger = winston.createLogger({
   format: winston.format.json(),
   defaultMeta: { service: "user-service" },
   transports: [
-    new winston.transports.Console(),
     new winston.transports.File({ filename: "error.log", level: "error" }),
   ],
 });
 
-const miFuncion = (val) => {
-  if (typeof val === "number") {
-    return 2 * val;
-  }
-  throw new Error("El valor debe ser de tipo número.");
-};
+function muestraError() {
+  throw new Error("Función que muestra Error");
+}
 
 try {
-  const doble = miFuncion("asd");
-  console.log(doble);
+  muestraError();
 } catch (e) {
-  logger.error(`${e}`);
+  logger.log("error", e.toString());
 }
