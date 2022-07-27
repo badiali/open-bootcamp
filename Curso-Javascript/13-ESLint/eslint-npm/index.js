b7980563b16b5ad1b471dfa111352767b367e158
Luis Badiali <luis@badiali.es>
2022-07-27 11:25:46 +0200

    13 - Uso de ESLint

---
+++ Curso-Javascript/13-ESLint/eslint-npm/index.js
@@ -0,0 +1,22 @@
+const nombre = "Luis"
+
+// Desactivación temporal de reglas
+
+/* eslint-disable */
+
+const persona2 = 'Mercedes'
+
+/* eslint-enable */
+
+// Desactivar por línea
+
+const persona3 = 'Milagros'; // eslint-disable-line
+
+/* eslint-disable-next-line indent */
+    const persona4 = "Juan"
+
+console.log("Hola")
+
+var persona5 = "Milagros"
+
+console.log(4)
---
+++ Curso-Javascript/13-ESLint/index.js
@@ -0,0 +1,4 @@
+var nombre = "Luis"
+
+     var nombre2 = "Mercedes";
+
