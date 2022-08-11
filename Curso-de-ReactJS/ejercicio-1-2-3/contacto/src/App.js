82633cfc31818a461b3c4e6a3350267cc40ec70a
Luis Badiali <luis@badiali.es>
2022-08-11 15:07:58 +0200

    Componente App

--- Curso-de-ReactJS/ejercicio-1-2-3/contacto/src/App.js
+++ Curso-de-ReactJS/ejercicio-1-2-3/contacto/src/App.js
@@ -1,10 +1,19 @@
 import "./App.css";
-import ContactListComponent from "./components/container/ContactList";
+import ComponenteA from "./components/ComponenteA";
+import { Contacto } from "./models/contacto.class";
 
 function App() {
+  const contactoPrueba = new Contacto(
+    "Luis",
+    "Badiali",
+    "luis@badiali.es",
+    true
+  );
+
   return (
     <div className="App">
-      <ContactListComponent />
+      <h1>Contacto</h1>
+      <ComponenteA contacto={contactoPrueba} />
     </div>
   );
 }
