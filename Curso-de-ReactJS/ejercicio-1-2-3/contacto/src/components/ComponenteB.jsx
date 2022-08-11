56127a8679d353637853e74938a3629f6480fcfb
Luis Badiali <luis@badiali.es>
2022-08-11 15:08:11 +0200

    Componente B

---
+++ Curso-de-ReactJS/ejercicio-1-2-3/contacto/src/components/ComponenteB.jsx
@@ -0,0 +1,23 @@
+import React, { useState } from "react";
+import PropTypes from "prop-types";
+
+const ComponenteB = (estado) => {
+  const [conectado, setConectado] = useState(estado);
+
+  return (
+    <div className="ComponenteB">
+      <p className={`${conectado ? "online" : "offline"}`}>
+        {conectado ? "Contacto en línea" : "Contacto No Disponible"}
+      </p>
+      <button onClick={() => setConectado(!conectado)}>
+        {conectado ? "Desconectar" : "Conectar"}
+      </button>
+    </div>
+  );
+};
+
+ComponenteB.propTypes = {
+  estado: PropTypes.bool,
+};
+
+export default ComponenteB;
