e4feb4a613c69a70bf03960dec61fddcb8e307f3
Luis Badiali <luis@badiali.es>
2022-08-11 20:07:26 +0200

    Importar el CSS del ComponenteB

--- Curso-de-ReactJS/ejercicio-1-2-3/contacto/src/components/ComponenteB.jsx
+++ Curso-de-ReactJS/ejercicio-1-2-3/contacto/src/components/ComponenteB.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import PropTypes from "prop-types";
+import "./ComponenteB.css";
 
 const ComponenteB = (estado) => {
   const [conectado, setConectado] = useState(estado);
